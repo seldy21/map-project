a42d5bc17925da750b90ae5cd8fd76d7f02f72fe
seldy21 <seldy@naver.com>
2025-05-14 22:16:50 +0900

    카카오맵 구현

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,15 +1,35 @@
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
 /* global kakao */
+
+import Script from "next/script";
 
 declare global {
   interface Window {
-    kakao: any;
+    kakao: {
+      maps: any; // Replace 'any' with specific types if available
+    };
   }
 }
 
 export default function Home() {
-  // kakao map 로드
-  const loadKakaoMap = () => {}
+  const loadKakaoMap = () => {
+    window.kakao.maps.load(function () {
+      const container = document.getElementById("map");
+      const options = {
+        center: new window.kakao.maps.LatLng(33.450701, 126.570667),
+        level: 3,
+      };
+      new window.kakao.maps.Map(container, options);
+    });
+  };
   return (
-      <div id="map" className="w-full h-screen">Map index page</div>
+    <>
+      <Script
+        type="text/javascript"
+        src={`//dapi.kakao.com/v2/maps/sdk.js?appkey=${process.env.NEXT_PUBLIC_KAKAO_CLIENT_ID}&libraries=services,clusterer,drawing&autoload=false`}
+        onReady={loadKakaoMap}
+      />
+      <div id="map" className="w-full h-screen bg-amber-50"></div>
+    </>
   );
 }
