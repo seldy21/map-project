56d7dc69282a5e330590ebb0120f74f0914e6993
seldy21 <seldy@naver.com>
2025-05-19 20:58:43 +0900

    build: node 버전 20.x.x로 수정 feat: prisma seed 시행

---
+++ prisma/seed.ts
@@ -0,0 +1,37 @@
+
+import * as data from '@/data/storeData.json';
+import { PrismaClient } from '@/generated/prisma';
+
+const prisma = new PrismaClient();
+
+async function seedData() {
+
+  data?.DATA?.map(async (store) => {
+    const storeData = {
+      phone: store.tel_no,
+      address: store.rdn_code_nm,
+      latitude: store.y_dnts,
+      longitude: store.x_cnts,
+      name: store.upso_nm,
+      category: store.bizcnd_code_nm,
+      storeType: store.cob_code_nm,
+      foodCertifyName: store.crtfc_gbn_nm
+    };
+
+    const res = await prisma.store.create({
+      data: storeData
+    });
+    console.log(`Created store: ${res}`);
+  });
+}
+
+export async function main() {
+  await seedData();
+}
+
+main().catch((e) => {
+  console.error(e);
+  process.exit(1);
+}).finally(() => {
+  prisma.$disconnect();
+})
